96f22680bd99790ee0d593773fa981b68e42d1b2
Clint Modien <cmodien@gmail.com>
2017-02-25 08:31:12 -0800

    testing docker build

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shinjin' });
+  res.render('index', { title: 'Shinjin 2' });
 });
 
 module.exports = router;
