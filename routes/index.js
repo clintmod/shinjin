2cd23161f45b6779fb937c85aa8d83a9037d9679
Clint Modien <cmodien@gmail.com>
2017-02-26 09:03:25 -0800

    Got it working with docker locally and auto deploying to docker cloud

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var neo4j = require('neo4j-driver').v1;
+var neo4jurl = process.env.NEO4J_ENV_DOCKERCLOUD_SERVICE_FQDN || "neo4j"
+var driver = neo4j.driver("bolt://" + neo4jurl, neo4j.auth.basic("neo4j", "asdf1234"));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shinjin 3' });
+  var session = driver.session();
+  session
+  .run( "MERGE (n {type:'counter'}) " +
+		"ON CREATE SET n.count = 1 " +
+		"ON MATCH SET n.count = n.count + 1 " +
+		"RETURN n.count" )
+  .then( function(result){
+  	var count = result.records[0].get("n.count");
+  	res.render('index', { title: 'Shinjin ' + count });
+  })
+  .catch(function(error) {
+  	res.render('index', {error:error})
+	console.log(error);
+  })
 });
 
 module.exports = router;
