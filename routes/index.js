7d350c82a744e5f8b89aa298ef5cc776b7df5cab
Clint Modien <cmodien@gmail.com>
2017-02-26 12:38:41 -0800

    adding require os

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var neo4j = require('neo4j-driver').v1;
 var neo4jurl = process.env.NEO4J_ENV_DOCKERCLOUD_SERVICE_HOSTNAME || "neo4j"
 var driver = neo4j.driver("bolt://" + neo4jurl, neo4j.auth.basic("neo4j", "asdf1234"));
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
